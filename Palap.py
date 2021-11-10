fec6648985719d47d161072369a3402cbb0ce1d2
PBence <90601408+PBencee@users.noreply.github.com>
2021-11-10 14:21:55 +0100

    Add files via upload

---
+++ Palap.py
@@ -0,0 +1,21 @@
+#1 Feladat
+print ("Gyertyák, fáklyák, okos szavak Törjetek és mutassatok megint tovább, megint tovább, Hogy utódunk lépjen elénk. - Nincs száz gyertyád, nincsen ezer? Hát eggyel hintsed, ontsd a fényt!")
+#2 Feladat
+nev = input ("Mi a neved?= ")
+szuletesido = str (input("Mikor születtél? = "))
+szulethely = input ("Hol Születtél? = ")
+lakcim = str (input("Mi a lakcímed? = "))
+print (nev,szuletesido,szulethely,lakcim,)
+#3 Feladat
+szam1 = int (input("Adj meg egy számot = "))
+szam2 = int (input("Adj meg egy számot megint ="))
+print (szam2,szam1,)
+#4 Feladat
+a = int (input("Adj meg egy számot = "))
+b = int (input("Adj meg egy számot megint ="))
+print ("T=",a*b, "K=",2*(a+b),)
+#5 Feladat
+a = int (input("Adj meg egy számot = "))
+b = int (input("Adj meg egy számot megint ="))
+c = int (input("Adj meg egy számot megint ="))
+print ("T=",a*b/2, "K=",a+b+c,)
